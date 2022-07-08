fe7d1e9bcea6ab0ba3eff205dceb3e6613b0e597
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-08 18:21:30 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import random
+import pyjokes
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+   return ("")
+
+@app.route('/joke')
+def joke():
+    
+
+
+    joke = pyjokes.get_joke(language="en")
+    return(joke)
+
+
+
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False,port=8000)
+   
+    
