2dd731a6717ec6e4635210db7cc2970966ad5c27
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-09-03 17:36:00 +0530

    Meme api done!

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def get_meme():
 
-    url = "<https://meme-api.herokuapp.com/gimme>"
+    url = "https://meme-api.herokuapp.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
